1dda826fb498027df219238d958aadc92a7e0a58
BrenoSVVeloso <bbreno.valadares@gmail.com>
2025-04-24 16:09:14 -0300

    feature/0001-starting_project

---
+++ src/main/java/com/clone/razerGold/entity/dto/PurchaseDTO.java
@@ -0,0 +1,28 @@
+package com.clone.razerGold.entity.dto;
+
+import com.clone.razerGold.entity.Game;
+import com.clone.razerGold.entity.enums.PayMethodEnum;
+import com.clone.razerGold.entity.enums.PriceEnum;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PurchaseDTO {
+    
+    private long id;
+
+    private Game game;
+
+    private PriceEnum price;
+
+    private PayMethodEnum payMethod;
+
+    private String email;
+
+    private String promotionalCode;
+}
---
+++ src/main/java/com/clone/razerGold/entity/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.clone.razerGold.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserDTO {
+    
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+}
